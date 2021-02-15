55de9aa7902deb93ec06810e0cf7e73b3e71a1f8
Dylan Gomer <36208962+dg1229@users.noreply.github.com>
2021-02-14 21:00:00 -0500

    Added page 2 & 3

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,11 +1,27 @@
 const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const page3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
 
-const getIndex = (request, response) => {
+const loadPage = (request, response, page) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
+  response.write(page);
   response.end();
 };
 
+const getIndex = (request, response) => {
+  loadPage(request, response, index);
+};
+
+const getPage2 = (request, response) => {
+  loadPage(request, response, page2);
+};
+
+const getPage3 = (request, response) => {
+  loadPage(request, response, page3);
+};
+
 module.exports.getIndex = getIndex;
+module.exports.getPage2 = getPage2;
+module.exports.getPage3 = getPage3;
